759f6ae02fb690ca58d882f74476a76f99cb9772
eric-zamora-tech <eric.zamora.in@gmail.com>
2024-09-26 15:14:48 -0500

    Update to currency bot

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+from discord.ext import commands
 from dotenv import load_dotenv
+import aiosqlite
 import discord
-import requests
-import random
-import json
 import os
 
 load_dotenv()
@@ -10,38 +9,29 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-sad_words = ['sad', 'depressed', 'unhappy', 'angry', 'miserable', 'depressing']
-
-starter_encouragements = [
-    'Cheer up!',
-    'Hang in there.',
-    'You are a great person / bot!'
-]
-
-def get_quote():
-    response = requests.get('https://zenquotes.io/api/random')
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + ' -' + json_data[0]['a']
-    return quote
-
-@client.event
+# Initialize the database
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    async with aiosqlite.connect('economy.db') as db:
+        await db.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, balance INTEGER)')
+        await db.commit()
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    msg = message.content
-    
-    if msg.startswith('$inspire'):
-        quote = get_quote()
-        await message.channel.send(quote)
-    
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
+@bot.command(name='balance')
+async def balance(ctx):
+    async with aiosqlite.connect('economy.db') as db:
+        async with db.execute('SELECT balance FROM users WHERE id = ?', (ctx.author.id,)) as cursor:
+            row = await cursor.fetchone()
+            if row is None:
+                await db.execute('INSERT INTO users (id, balance) VALUES (?,?)', (ctx.author.id, 100))
+                await db.commit()
+                balance = 100
+            else:
+                balance = row[0]
+        await ctx.send(f'{ctx.author.mention} has a balance of {balance} coins.')
 
-client.run(os.getenv('BOT_TOKEN'))
+
+
+bot.run(os.getenv('BOT_TOKEN'))
